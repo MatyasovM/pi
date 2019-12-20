b17ba255bd19735e70aadc316eedbc0cf2986464
MikhailMatyasov <matyasov.mixail@gmail.com>
2019-12-20 17:29:36 +0300

    Added knap

---
+++ KnapsackProblem/KnapsackProblem/Knapsack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace KnapsackProblem
+{
+    public class KnapsackObject
+    {
+        public int ID { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+    }
+    
+    public class Knapsack
+    {
+        List <KnapsackObject> myKnapsack = new List<KnapsackObject>();
+
+        public List <KnapsackObject> Knapsck { get => myKnapsack; set => myKnapsack = value; }
+
+        public bool Read (string theFileName)
+        {
+            var aFileStream = File.OpenRead (theFileName);
+            if (aFileStream == null) {
+                return false;
+            }
+            StreamReader aReader = new StreamReader (aFileStream);
+
+            string aLine = "";
+            while ((aLine = aReader.ReadLine()) != null) {
+                
+                KnapsackObject anObj = new KnapsackObject();
+                int i = 0;
+                anObj.ID = GetNextValue (ref i, aLine);
+                anObj.Price = GetNextValue (ref i, aLine);
+                anObj.Weight = GetNextValue (ref i, aLine);
+                
+                myKnapsack.Add (anObj);
+            }
+            return true;
+        }
+
+        int GetNextValue (ref int theCurPos, string theCurrentLine)
+        {
+            string aCurrentValue = "";
+            for (; theCurPos < theCurrentLine.Length; theCurPos++) {
+                if (theCurrentLine[theCurPos] == ' ') {
+                    theCurPos++;
+                    break;
+                }
+                aCurrentValue += theCurrentLine[theCurPos];
+            }
+            return System.Convert.ToInt32 (aCurrentValue);
+        }
+    }
+}
---
+++ KnapsackProblem/KnapsackProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace KnapsackProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knapsack aKnap = new Knapsack();
+            aKnap.Read (@"C:\Users\mmatyasov\Desktop\pi\KnapsackProblem\KnapsackProblem\Knapsack#1.txt");
+
+
+            
+        }
+    }
+}
