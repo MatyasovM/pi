ed59e889f5a91d8cd160b25d2ad8ddbbc7ffa73d
MikhailMatyasov <matyasov.mixail@gmail.com>
2019-12-30 12:33:47 +0300

    All was done

--- Exercise/Exercise/Lab10.cs
+++ Exercise/Exercise/Lab10.cs
@@ -16,19 +16,139 @@ namespace Exercise1
     class Lab10
     {
         List <KnapsackObject> myKnapsack = new List<KnapsackObject>();
+        int myLimitWeight = 106;
 
         public void Do()
         {
+            myKnapsack.Sort ((theLeft, theRight) => {
+                if (theLeft.Price > theRight.Price) {
+                    return 1;
+                } else if (theLeft.Price < theRight.Price) {
+                    return -1;
+                }
+                return 0;
+            });
             Read ("Knapsack#1.txt");
-            var aParent1 = GenerateParent (106);
+            int aCurrentMaxPrice = 0;
+            var aParent1 = GenerateParent (myLimitWeight);
+            var aParent2 = GenerateRandomParent (myLimitWeight);
+            int aParent1Price = GetPrice (aParent1);
+            int aParent2Price = GetPrice (aParent2);
+            if (aParent1Price > aParent2Price) {
+                aCurrentMaxPrice = aParent1Price;
+            } else {
+                aCurrentMaxPrice = aParent2Price;
+            }
 
-            Read ("Knapsack#2.txt");
-            var aParent2 = GenerateParent (88);
+            int aNumberAttempts = 0;
+            int aNumberOfGeneration = 0;
+            while (true) {
+                Console.WriteLine ($"A generation #{aNumberOfGeneration}");
+                if (aNumberAttempts > 2) {
+                    break;
+                }
+                
+                var aNextPop = GetNextPopulation (aParent1, aParent2);
+                var aNewMaxPrice = GetPrice (aNextPop[0]);
+                if (aNewMaxPrice > aCurrentMaxPrice) {
+                    aCurrentMaxPrice = aNewMaxPrice;
+                    aParent1 = aNextPop[0];
+                    aParent2 = aNextPop[1];
+                    aNumberAttempts = 0;
+                } else {
+                    aNumberAttempts++;
+                }
+                aNumberOfGeneration++;
+            }
 
-            int aNumberOfChilds = 10;
-            var aStartPopulation = MakePopulation (aParent1, aParent2, aNumberOfChilds);
+            Console.WriteLine ($"Max price: {aCurrentMaxPrice}");
+            Console.Write ("Knapsack: ");
+            PrintApplicant (aParent1);
+
+        }
+
+        void PrintApplicant (List <int> thePplicant)
+        {
+            foreach (var anItem in thePplicant) {
+                Console.Write (anItem + " ");
+            }
+            Console.WriteLine ("");
+        }
+
+        private List <List <int>> GetNextPopulation (List <int> theParent1, List <int> theParent2)
+        {
+            Console.WriteLine ("Please enter number of childs in new population");
+            int aNumberOfChilds = Convert.ToInt32 (Console.ReadLine());
+            var aStartPopulation = MakePopulation (theParent1, theParent2, aNumberOfChilds);
             var aMutatedPopulation = ApplyMutation (aStartPopulation);
-            aNumberOfChilds = 0;
+            Console.WriteLine ("New population: \n");
+            foreach (var anApplic in aMutatedPopulation) {
+                PrintApplicant (anApplic);
+            }
+            var aBestChilds = ChooseTheBest (aMutatedPopulation, myLimitWeight);
+            
+            Console.Write ("The best child: ");
+            if (aBestChilds.Count != 0) {
+                PrintApplicant (aBestChilds[0]);   
+            } else {
+                Console.Write ("Non\n");
+            }
+
+            List <List <int>> aNextApplicants = new List <List <int>>();
+            aNextApplicants.Add (theParent1);
+            aNextApplicants.Add (theParent2);
+            aNextApplicants.AddRange (aBestChilds);
+            return ChooseTheBest (aNextApplicants, myLimitWeight);            
+        }
+
+        private List <List <int>> ChooseTheBest (List <List <int>> theApplicants, int theLimitWeight)
+        {
+            theApplicants.Sort ((theLeft, theRight) => {
+                int aLeftPrice = GetPrice (theLeft);
+                int aRightPrice = GetPrice (theRight);
+                if (aLeftPrice > aRightPrice) {
+                    return 1;
+                } else if (aLeftPrice < aRightPrice) {
+                    return -1;
+                }
+
+                return 0;
+            });
+
+            List <List <int>> aRes = new List <List <int>>();
+            foreach (var aChild in theApplicants) {
+                if (GetWeigth (aChild) <= theLimitWeight) {
+                    aRes.Add (aChild);
+                }
+            }
+            if (aRes.Count > 1) {
+                return aRes.GetRange (0, 2);   
+            }
+            return aRes;
+        }
+
+        private int GetPrice (List <int> theKnap)
+        {
+            int aPrice = 0;
+            for (int i = 0 ; i < theKnap.Count; i++) {
+                if (theKnap[i] == 1) {
+                    aPrice += myKnapsack[i].Price;
+                }
+            }
+
+            return aPrice;
+        }
+
+        private int GetWeigth (List <int> theKnap)
+        {
+            int aWeight = 0;
+            for (int i = 0 ; i < theKnap.Count; i++) {
+                if (theKnap[i] == 1) {
+                    aWeight += myKnapsack[i].Weight;
+                }
+            }
+
+            return aWeight;
         }
 
         public bool Read (string theFileName)
@@ -98,6 +218,25 @@ namespace Exercise1
             }
 
             return S;
+        }
+
+        public List <int> GenerateRandomParent (int theWeightLimit)
+        {
+            List <int> aRes = Enumerable.Repeat (0, myKnapsack.Count).ToList();
+            int aCurrentWeight = 0;
+            for (int i = 0 ; i < myKnapsack.Count; i++) {
+                Random aRand = new Random ((int) DateTime.UtcNow.Ticks);
+                int aNext = aRand.Next (0, 2);
+                if (aNext == 1) {
+                    if (aCurrentWeight + myKnapsack[i].Weight > theWeightLimit) {
+                        break;
+                    }
+                    aCurrentWeight += myKnapsack[i].Weight;
+                }
+                aRes[i] = aNext;
+            }
+
+            return aRes;
         }
 
         private KnapsackObject GetMax_C (List <KnapsackObject> theKnap)
